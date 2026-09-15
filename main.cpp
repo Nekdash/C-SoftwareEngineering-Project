ae25649a59357042c47ac88b03c96865163a2127
Nekrasova Darya <_>
2026-09-15 21:28:12 +0300

    library, CI/CD via github workflows

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "add.hpp"
 
-int main(){
-    std::cout << "Hello, world!\n";
-    std::cout << "Nice to meet you\n";
-    std::cout << "Bye bye!\n";
+int main(int argc, char *argv[]){
+    if (argc != 3){
+        std::cerr << "Exactly 2 arguments required\n";
+        return 1;
+    }
+    int a = std::atoi(argv[1]);
+    int b = std::atoi(argv[2]);
+    std::cout << add(a, b) << "\n";
     return 0;
 }
